7d440f69b9e1b3d78c5760acb17ef4e547b51110
Michele Santos <santosmichele.contato@gmail.com>
2022-04-08 00:55:11 -0300

    Finalização das condicionais e funções da rodada

--- jogo.py
+++ jogo.py
@@ -1,5 +1,6 @@
 import controle_rodada
 import iniciar
+import finalizar
 
 
 def jogar():
@@ -12,61 +13,53 @@ def jogar():
         amarelo = iniciar.dado_intermediario()
         verde = iniciar.dado_facil()
         tubo = iniciar.receber_tubo()
+
         dados = 3
         jogada = 0
+
         placar = {"cerebro": 0, "passo": 0, "tiro": 0}
         start = iniciar.fazer_jogada()
 
         while start:
             controle_rodada.exibir_status(jogada, vermelho, amarelo, verde)
-            cores = []
+            faces = []
+
             for _ in range(dados):
                 sorteado = controle_rodada.lancar_dados()
                 cor = controle_rodada.verificar_cor(sorteado, vermelho, amarelo, verde)
-                cores.append(cor)
                 controle_rodada.desenhar_dado(cor)
                 face = controle_rodada.verificar_face(sorteado)
+                print(face)
+                faces.append(face)
                 tubo = controle_rodada.remover_dado(tubo, sorteado, face)
-                print(len(tubo))
-                placar_atual = pontuar_jogada(placar, face)
-                print(placar_atual)
-                dano = marcar_tiro(placar_atual)
-                if dano < 3:
-                    placar = placar_atual
+                placar = finalizar.pontuar_jogada(placar, face)
+                print(placar)
 
+            print(f"Restam {len(tubo)} dados!")
+            print(placar)
+            dano = finalizar.marcar_tiro(placar)
+            print(dano)
+
+            if dano < 3:
+                finalizar.mensagem_pontuacao()
+                finalizar.placar_cerebro(placar)
+                finalizar.placar_passo(placar)
+                finalizar.placar_tiro(placar)
+
+                continuar = finalizar.jogar_novamente()
+                restam = len(tubo)
+
+                if continuar == "S":
+                    finalizar.contar_dados(restam)
                 else:
-                    print("Mensagem fim de turno e reinicializacao")
-
-            break
-
-
-def pontuar_jogada(placar, face):
-    face_sorteada = face
-    placar_atual = placar
-    if face_sorteada == "C":
-        placar_atual["cerebro"] += 1
-    elif face_sorteada == "T":
-        placar_atual["tiro"] += 1
-    elif face_sorteada == "P":
-        placar_atual["passo"] += 1
-    return placar_atual
-
-
-def marcar_tiro(placar_atual):
-    dano_sofrido = placar_atual.get("tiro")
-    return dano_sofrido
-
-
-def mensagem_pontuacao(start, placar_atual):
-    placar = placar_atual
-    cerebro = placar.get("cerebro")
-    tiro = placar.get("tiro")
-    passo = placar.get("passo")
-    print(cerebro)
-    print(tiro)
-    print(passo)
-    start = False
-
+                    start = False
+                    placar["tiro"] = 0
+                    placar["passo"] = 0
+            else:
+                print("Morrer... de novo! Você perdeu todos os pontos. ")
+                print("Aguarde o próximo turno para jogar. ")
+                start = False
+                placar = {"cerebro": 0, "tiro": 0, "passo": 0}
 
 
 if __name__ == "__main__":
