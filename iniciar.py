9ccc4900f8b34cdd8769c73617ebffe4abd133d7
Michele Santos <santosmichele.contato@gmail.com>
2022-04-08 23:01:41 -0300

    Add do desenho do zumbi. Correcao da condicional para inicio do turno. Add de separadores e alinhamento

--- iniciar.py
+++ iniciar.py
@@ -1,9 +1,11 @@
 def contar_jogadores():
     while True:
         num_jogadores = int(input("Informe o número de jogadores: "))
+        print("=" * 80)
         if num_jogadores <= 1:
-            print("Necessário dois ou mais jogadores para iniciar a partida.")
-            print("Convoque mais zumbis!")
+            print("Necessário dois ou mais jogadores para iniciar a partida.".center(80))
+            print("Convoque mais zumbis!".center(80))
+            print("=" * 80)
             continue
         else:
             break
@@ -17,6 +19,7 @@ def cadastrar_jogadores(num_jogadores):
         for i in range(numero):
             nome = input(f"Digite o nome do {i+1}º jogador: ").upper().strip()
             nomes.append(nome)
+        print("="*80)
     return nomes
 
 
@@ -30,9 +33,17 @@ def salvar_nome(jogadores):
 
 def listar_jogadores(nomes):
     nome_jogadores = tuple(nomes)
-    for i in range(len(nomes)):
-        print(f"{i+1}º Jogador: {nomes[i]}")
-    print("Preparem-se para a partida!")
+    print("            ______________            ".center(80))
+    print("          /                \          ".center(80))
+    print("  _____  |   ===      ===   |  _____  ".center(80))
+    print(" /     \  \        \/       / /     \ ".center(80))
+    print(" | | | <|  \    VVVVVVV    /  |> | | |".center(80))
+    print(" V V V  \   |   |     |   |   /  V V V".center(80))
+    print("     \   \  |   |     |   |  /   /    ".center(80))
+    print("      \   \ \   \^^^^^/   | /   /     ".center(80))
+    print("       \     -------------     /      ".center(80))
+    print(f"      PREPAREM-SE PARA A PARTIDA!    ".center(80), '\n')
+    print("="*80)
     return nome_jogadores
 
 
@@ -64,9 +75,18 @@ def receber_tubo():
 
 
 def fazer_jogada():
-    lancar_dados = input("Iniciar jogada? Digite S para Sim e N para Não: ").upper().strip()
-    if lancar_dados == "S":
-        jogando = True
-    else:
-        jogando = False
+    jogando = True
+    while True:
+        lancar_dados = input("Iniciar jogada? Digite S para Sim e N para Não: ").upper().strip()
+        print("="*80)
+        if lancar_dados == "S":
+            jogando = True
+            break
+        else:
+            continue
+    print(jogando)
     return jogando
+
+
+if __name__ == "__main__":
+    fazer_jogada()
