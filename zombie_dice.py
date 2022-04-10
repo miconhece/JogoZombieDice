5c7411f2da6166561cc64602317d4b56731ff705
Michele Santos <santosmichele.contato@gmail.com>
2022-04-10 16:44:59 -0300

    condicional de fim de jogo

--- zombie_dice.py
+++ zombie_dice.py
@@ -2,6 +2,7 @@ import controle_rodada
 import iniciar
 import finalizar
 import painel
+import classificacao
 
 
 def introducao():
@@ -12,94 +13,91 @@ def jogar():
     ranking = {}
     boas_vindas()
     numero = iniciar.contar_jogadores()
-    print(numero)
     jogadores = tuple(iniciar.cadastrar_jogadores(numero))
-    print(jogadores)
-    iniciar.preparar_jogadores()
 
-    for i in range(len(jogadores)):
-        # PEGAR TUBO #
-        jogador = jogadores[i]
-        ranking[jogador] = 0
-        print(jogador)
+    partida = True
+    rodada = 0
 
-        iniciar.iniciar_turno(jogador)
+    while partida:
 
-        vermelho = iniciar.dado_dificil()
-        print(vermelho)
-        amarelo = iniciar.dado_intermediario()
-        print(amarelo)
-        verde = iniciar.dado_facil()
-        print(verde)
-        tubo = iniciar.receber_tubo()
-        print(tubo)
+        rodada = rodada + 1
+        iniciar.preparar_jogadores()
 
-        dados = 3
-        jogada = 0
-
-        placar = {"cerebro": 0, "passo": 0, "tiro": 0}
-        start = iniciar.fazer_jogada()
-        print(start)
-
-        while start:
-            jogada = jogada + 1
-            controle_rodada.exibir_status(tubo, jogada, vermelho, amarelo, verde)
-            faces = []
-
-            for _ in range(dados):
-
-                sorteado = controle_rodada.lancar_dados()
-                print(sorteado)
-                cor = controle_rodada.verificar_cor(sorteado, vermelho, amarelo, verde)
-                print(cor)
-                controle_rodada.desenhar_dado(cor)
-
-                face = controle_rodada.verificar_face(sorteado)
-                print(face)
-                faces.append(face)
-
-                tubo = controle_rodada.remover_dado(tubo, sorteado)
-                print(tubo)
-                if face == "P":
-                    tubo.append(sorteado)
-
-                placar = finalizar.pontuar_jogada(placar, face)
-                print(placar)
-
-            controle_rodada.parcial_tubo_cont(tubo)
-
-            dano = finalizar.marcar_tiro(placar)
-            print(dano)
-            pontuacao = finalizar.acumulado(placar)
-            print(pontuacao)
-
-            if dano < 3:
-                finalizar.mensagem_pontuacao()
-                finalizar.placar_cerebro(pontuacao)
-                pontos = finalizar.marcar_pontos(placar)
-                finalizar.placar_passo(pontuacao)
-                finalizar.placar_tiro(pontuacao)
-
-                continuar = finalizar.jogar_novamente()
-                print(continuar)
-                restam = len(tubo)
-
-                if continuar == "S":
-                    finalizar.contar_dados(restam)
-                else:
-                    start = False
-            else:
-                print("Morrer... de novo! Você perdeu todos os pontos. ")
-                print("Aguarde o próximo turno para jogar. ")
-                start = False
-                placar = {"cerebro": 0, "tiro": 0, "passo": 0}
+        for i in range(len(jogadores)):
+            # PEGAR TUBO #
+            jogador = jogadores[i]
+            if rodada == 1:
                 pontos = 0
+                ranking[jogador] = pontos
+            else:
+                pontos = ranking[jogador]
+                ranking[jogador] = pontos
 
-            ranking[jogador] = pontos
-            placar_final = placar
-            print(placar_final)
-            finalizar.final_turno(start)
-    print(ranking)
+            iniciar.iniciar_turno(jogador)
+
+            vermelho = iniciar.dado_dificil()
+            amarelo = iniciar.dado_intermediario()
+            verde = iniciar.dado_facil()
+            tubo = iniciar.receber_tubo()
+
+            dados = 3
+            jogada = 0
+
+            placar = {"cerebro": 0, "passo": 0, "tiro": 0}
+            start = iniciar.fazer_jogada()
+
+            while start:
+                jogada = jogada + 1
+                controle_rodada.exibir_status(tubo, jogada, vermelho, amarelo, verde)
+                faces = []
+
+                for _ in range(dados):
+
+                    sorteado = controle_rodada.lancar_dados()
+                    cor = controle_rodada.verificar_cor(sorteado, vermelho, amarelo, verde)
+                    controle_rodada.desenhar_dado(cor)
+
+                    face = controle_rodada.verificar_face(sorteado)
+                    faces.append(face)
+
+                    tubo = controle_rodada.remover_dado(tubo, sorteado)
+                    if face == "P":
+                        tubo.append(sorteado)
+
+                    placar = finalizar.pontuar_jogada(placar, face)
+
+                controle_rodada.parcial_tubo_cont(tubo)
+
+                dano = finalizar.marcar_tiro(placar)
+                pontuacao = finalizar.acumulado(placar)
+
+                if dano < 3:
+                    finalizar.mensagem_pontuacao()
+                    finalizar.placar_cerebro(pontuacao)
+                    pontos = finalizar.marcar_pontos(placar)
+                    ranking[jogador] += pontos
+                    finalizar.placar_passo(pontuacao)
+                    finalizar.placar_tiro(pontuacao)
+
+                    continuar = finalizar.jogar_novamente()
+                    restam = len(tubo)
+
+                    if continuar == "S":
+                        finalizar.contar_dados(restam)
+                    else:
+                        start = False
+                else:
+                    print("Morrer... de novo! Você perdeu todos os pontos. ")
+                    print("Aguarde o próximo turno para jogar. ")
+                    start = False
+                    placar = {"cerebro": 0, "tiro": 0, "passo": 0}
+                    pontos = 0
+
+                    ranking[jogador] += pontos
+
+                finalizar.final_turno(start)
+
+        print(ranking)
 
 
 def boas_vindas():
