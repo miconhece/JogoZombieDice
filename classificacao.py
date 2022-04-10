94f54c97c7be6d0cb6e3653c6b9df040d6532694
Michele Santos <santosmichele.contato@gmail.com>
2022-04-10 18:13:11 -0300

    declaracao da funcao de fim de jogo

---
+++ classificacao.py
@@ -0,0 +1,33 @@
+def vencedor(ranking, rodada):
+    n_rodada = rodada
+    if n_rodada > 0:
+        dic = ranking
+        lista = dic.values()
+        lista_pontos = []
+        lista_pontos.extend(lista)
+        print(lista_pontos)
+        maior = -10000
+        for i in lista_pontos:
+            if i > maior:
+                maior = i
+        if maior > 12:
+            partida = False
+            print("Jogo Encerrado! Já temos um ganhador".center(80).upper())
+            print("="*80)
+        else:
+            partida = True
+        return partida
+
+
+def classificacao_geral(ranking, partida):
+    jogadores = ranking
+    jogando = partida
+    if not jogando:
+        for jogador in jogadores:
+            print(f"{jogador} - Fez {jogadores[jogador]} pontos")
+    print("="*80)
+    print("Obrigada por jogar Zombie Dice".center(80))
+
+
+
+
